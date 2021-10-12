dd9b041505fb69fc5e709434b791f23a79c4a72c
Yan Mingzhi <realraindust@gmail.com>
2021-10-12 11:09:31 +0800

    add clean sub-command

--- tools.sh
+++ tools.sh
@@ -7,6 +7,8 @@ if [ $SUB_COMMAND = "logs" ]; then
 	sudo docker-compose logs -f
 elif [ $SUB_COMMAND = "stop" ]; then
 	sudo docker-compose down
+elif [ $SUB_COMMAND = "clean" ]; then
+	rm -rf .layer1/share/tea-camellia/chains/tea-layer1/db
 else
 	echo "supported subcommands: logs, stop"
 fi
