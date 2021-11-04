2115028a13ab6907873ad3c65b9e9ae2d12333bd
Yan Mingzhi <realraindust@gmail.com>
2021-11-04 19:59:43 +0800

    fix if condition error

--- install.sh
+++ install.sh
@@ -122,7 +122,7 @@ pre_settings() {
 }
 
 MEM_SIZE=`grep MemTotal /proc/meminfo | awk '{printf "%.0f", ($2 / 1024)}'`
-if [[ "$MEM_SIZE" -lt 1800 ]]; then
+if [ "$MEM_SIZE" -lt 1800 ]; then
   error "Machine memory size should larger equal than 2G"
   exit 1
 fi
