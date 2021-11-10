2e5279610f3437e78b37bb63b7fa1a99ea7f06e5
Yan Mingzhi <realraindust@gmail.com>
2021-11-10 12:32:50 +0800

    improve print and store values in .tea

--- gen_tea_id.sh
+++ gen_tea_id.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+TEA_CONFIG="$HOME/.tea"
+
 sudo apt-get install -y wget
 # mock-tea-id-generator compiled from https://github.com/tearust/mock-tea-id-generator
 wget https://raw.githubusercontent.com/tearust/delegator-resources/epoch6/mock-tea-id-generator
@@ -9,12 +11,47 @@ sudo chmod +x mock-tea-id-generator
 wget https://raw.githubusercontent.com/tearust/delegator-resources/epoch6/subkey
 sudo chmod +x subkey
 
+hint() {
+	echo "//////////////////// copy it //////////////////////"
+	echo "/ $HINT_VAR"
+	echo "///////////////////////////////////////////////////"
+}
+
+rm -f $TEA_CONFIG
+touch $TEA_CONFIG
+
 echo "-------- begin of machine id generation --------"
-./mock-tea-id-generator
+TEMP_TEA_ID_PATH=/tmp/gen_tea.temp
+
+./mock-tea-id-generator > $TEMP_TEA_ID_PATH
+TEA_ID_HEX=`sed '2q;d' $TEMP_TEA_ID_PATH`
+TEA_ID_BASE64=`sed '5q;d' $TEMP_TEA_ID_PATH`
+
+HINT_VAR=$TEA_ID_HEX
+hint
+
+echo "MY_TEA_ID=$TEA_ID_BASE64" > $TEA_CONFIG
+
+rm $TEMP_TEA_ID_PATH
 echo "-------- generation completed --------"
 
 echo ""
 
 echo "-------- begin of layer1 account generation --------"
-./subkey generate --scheme sr25519
+TEMP_LAYER1_PATH=/tmp/gen_layer1.temp
+
+./subkey generate --scheme sr25519 > $TEMP_LAYER1_PATH
+
+sed -ri "s@(\s*)(SS58 Address:\s*)@@" $TEMP_LAYER1_PATH
+sed -ri "s@(\s*)(Secret phrase:\s*)@@" $TEMP_LAYER1_PATH
+
+LAYER1_PHASE=`sed '1q;d' $TEMP_LAYER1_PATH`
+LAYER1_ADDRESS=`sed '6q;d' $TEMP_LAYER1_PATH`
+
+HINT_VAR=$LAYER1_ADDRESS
+hint
+
+echo "MY_LAYER1_ACCOUNT=$LAYER1_PHASE" >> $TEA_CONFIG
+
+rm $TEMP_LAYER1_PATH
 echo "-------- generation completed --------"
