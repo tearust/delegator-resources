feecca1d1948ec0686ed5b13636bf79ddd48ea2f
Yan Mingzhi <realraindust@gmail.com>
2021-11-11 14:43:26 +0800

    fix typo

--- gen_tea_id.sh
+++ gen_tea_id.sh
@@ -20,7 +20,7 @@ hint() {
 rm -f $TEA_CONFIG
 touch $TEA_CONFIG
 
-echo "-------- begin of machine id generation --------"
+echo "-------- begin machine id generation --------"
 TEMP_TEA_ID_PATH=/tmp/gen_tea.temp
 
 ./mock-tea-id-generator > $TEMP_TEA_ID_PATH
@@ -37,7 +37,7 @@ echo "-------- generation completed --------"
 
 echo ""
 
-echo "-------- begin of layer1 account generation --------"
+echo "-------- begin layer1 account generation --------"
 TEMP_LAYER1_PATH=/tmp/gen_layer1.temp
 
 ./subkey generate --scheme sr25519 > $TEMP_LAYER1_PATH
